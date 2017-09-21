0de97220d4437d8739c8abe0ec4ad8937e291955
subo <subo@ucredit.com>
2017-09-21 18:53:10 +0800

    dynamic change cron

--- src/main/java/com/common/quartz/ColorJob.java
+++ src/main/java/com/common/quartz/ColorJob.java
@@ -19,16 +19,18 @@ public class ColorJob implements Job {
 
     public static final String EXECUTION_COUNT = "executionCount";
 
-    private int _counter = 1;
+    private int counter = 1;
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         JobDataMap data = context.getJobDetail().getJobDataMap();
         String favoriteColor = data.getString(FAVORITE_COLOR);
         int count = data.getInt(EXECUTION_COUNT);
+        count++;
+        data.put(EXECUTION_COUNT, count);
         System.out.println("ColorJob: " + context.getJobInstance().toString() + " executing at " + new Date() + "\n" +
                 "  favorite color is " + favoriteColor + "\n" +
                 "  execution count (from job map) is " + count + "\n" +
-                "  execution count (from job member variable) is " + _counter);
+                "  execution count (from job member variable) is " + counter);
     }
 }
--- src/main/java/com/common/quartz/QuartzManager.java
+++ src/main/java/com/common/quartz/QuartzManager.java
@@ -1,5 +1,6 @@
 package com.common.quartz;
 
+import jdk.nashorn.internal.scripts.JO;
 import org.quartz.*;
 import org.quartz.impl.JobDetailImpl;
 import org.quartz.impl.StdSchedulerFactory;
@@ -21,6 +22,7 @@ public class QuartzManager {
     public static void main(String[] args) {
         //simpleTrigger();
         cronTrigger();
+        //paramsAndState();
     }
 
     public static void simpleTrigger() {
@@ -33,10 +35,11 @@ public class QuartzManager {
             Calendar calendar = Calendar.getInstance();
             calendar.add(Calendar.MINUTE, 1);
             Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").startAt(calendar.getTime()).build();
+            //scheduler.deleteJob(new JobKey("job1", "group1"));
             scheduler.scheduleJob(job, trigger);
             scheduler.start();
-            Thread.sleep(70*1000);
-            scheduler.shutdown();
+            Thread.sleep(70 * 1000);
+            scheduler.shutdown(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -46,17 +49,50 @@ public class QuartzManager {
         try {
             SchedulerFactory schedulerFactory = new StdSchedulerFactory();
             Scheduler scheduler = schedulerFactory.getScheduler();
-            JobDetail jobDetail = JobBuilder.newJob(QuartzJobExample.class).withIdentity("job1", "group1").build();
+            JobDetail jobDetail = JobBuilder.newJob(QuartzJobExample.class).withIdentity("job2", "group1").build();
             CronExpression cronExpression = new CronExpression("0/20 * * * * ?");
             cronExpression.setTimeZone(TimeZone.getTimeZone("GMT+8"));
-            CronTrigger cronTrigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").withSchedule(CronScheduleBuilder.cronSchedule(cronExpression)).build();
-            scheduler.deleteJob(new JobKey("job1", "group1"));
+            CronTrigger cronTrigger = TriggerBuilder.newTrigger().withIdentity("trigger2", "group1").withSchedule(CronScheduleBuilder.cronSchedule(cronExpression).withMisfireHandlingInstructionIgnoreMisfires()).build();
+            scheduler.pauseJob(new JobKey("job2", "group1"));
+            scheduler.deleteJob(new JobKey("job2", "group1"));
             scheduler.scheduleJob(jobDetail, cronTrigger);
             scheduler.start();
-            Thread.sleep(90*1000);
-            scheduler.shutdown();
+            Thread.sleep(90 * 1000);
+            //scheduler.shutdown(true);
+            //scheduler = schedulerFactory.getScheduler();
+            CronExpression cronExpressionNew = new CronExpression("0/10 * * * * ?");
+            cronExpression.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+            CronTrigger cronTriggerNew = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1").withSchedule(CronScheduleBuilder.cronSchedule(cronExpressionNew).withMisfireHandlingInstructionIgnoreMisfires()).forJob(jobDetail).build();
+            scheduler.rescheduleJob(cronTrigger.getKey(), cronTriggerNew);
+            scheduler.start();
+            Thread.sleep(90 * 1000);
+            scheduler.shutdown(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void paramsAndState() {
+        try {
+            SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            Scheduler scheduler = schedulerFactory.getScheduler();
+            JobDetail jobDetail1 = JobBuilder.newJob(ColorJob.class).withIdentity("job3", "group1").build();
+            SimpleTrigger simpleTrigger1 = TriggerBuilder.newTrigger().withIdentity("trigger3", "group1").startAt(new Date()).withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(10).withRepeatCount(4).withMisfireHandlingInstructionIgnoreMisfires()).build();
+            jobDetail1.getJobDataMap().put(ColorJob.FAVORITE_COLOR, "Green");
+            jobDetail1.getJobDataMap().put(ColorJob.EXECUTION_COUNT, "1");
+
+            JobDetail jobDetail2 = JobBuilder.newJob(ColorJob.class).withIdentity("job4", "group1").build();
+            SimpleTrigger simpleTrigger2 = TriggerBuilder.newTrigger().withIdentity("trigger4", "group1").startAt(new Date()).withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(10).withRepeatCount(4).withMisfireHandlingInstructionIgnoreMisfires()).build();
+            jobDetail2.getJobDataMap().put(ColorJob.FAVORITE_COLOR, "Red");
+            jobDetail2.getJobDataMap().put(ColorJob.EXECUTION_COUNT, 1);
+            scheduler.scheduleJob(jobDetail1, simpleTrigger1);
+            scheduler.scheduleJob(jobDetail2, simpleTrigger2);
+            scheduler.start();
+            Thread.sleep(60*1000);
+            scheduler.shutdown(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
