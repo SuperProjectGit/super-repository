49651c4edcda19c407de702c52a37321dc1be85b
super <subosuper@hotmail.com>
2017-12-04 22:16:45 +0800

    add spring customer resolver

---
+++ src/main/java/com/common/CustomResolver.java
@@ -0,0 +1,25 @@
+package com.common;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * @author super
+ * Create time 2017/12/4 22:11
+ */
+@Component
+public class CustomResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return false;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        return null;
+    }
+}
