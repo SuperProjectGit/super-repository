9412183edb4144e92e8671e4659dbc55e9a47ac1
subo <subo@ucredit.com>
2017-12-18 16:37:36 +0800

    java read large file

--- src/main/java/com/common/io/BigFileInput.java
+++ src/main/java/com/common/io/BigFileInput.java
@@ -2,16 +2,19 @@ package com.common.io;
 
 import com.common.concurrent.CommonThreadPool;
 import com.common.concurrent.MultiJob;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.temporal.ChronoField;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * 大文件读取
@@ -32,9 +35,8 @@ public class BigFileInput {
             scanner = new Scanner(inputStream, "UTF-8");
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                //appendFile("F:\\app.log", line);
-                System.out.println(line);
-                //logger.info(line);
+                appendFile("F:\\app.log", line);
+                logger.info(line);
             }
         } catch (Exception e) {
             logger.error("scanner file error", e);
@@ -57,7 +59,8 @@ public class BigFileInput {
         FileWriter fileWriter = null;
         try {
             fileWriter = new FileWriter(path, true);
-            fileWriter.write(content);
+            // 此处必须添加换行符，否则将认为是同一行，导致一个大数据行，在按行读取的时候可能会出现内存溢出
+            fileWriter.append(content).append("\r\n");
         } catch (IOException e) {
             logger.error("append file error", e);
         } finally {
@@ -72,19 +75,27 @@ public class BigFileInput {
     }
 
     public static void main(String[] args) {
-        String path = "F:\\app.log.2017-09-12";
-        Instant instantbefore = Clock.systemUTC().instant();
-        scannerFile(path);
-        Instant instantAfter = Clock.systemUTC().instant();
-        long ti = instantAfter.getLong(ChronoField.MILLI_OF_SECOND) - instantbefore.getLong(ChronoField.MILLI_OF_SECOND);
-        logger.info(String.valueOf(ti));
-        /*for (int i= 0; i< 300; i++) {
-            CommonThreadPool.asyncExecute(() -> scannerFile(path));
-            try {
-                Thread.sleep(2000);
-            } catch (Exception e) {
-                logger.error(e.getMessage());
-            }
-        }*/
+        String path = "F:\\app.log";
+        Long before = System.currentTimeMillis();
+        System.out.println("before:" + before);
+        //scannerFile(path);
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        for (int i= 0; i< 5; i++) {
+            CommonThreadPool.asyncExecute(() -> {
+                for (int j= 0; j< 512; j++) {
+                    scannerFile(path);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            logger.info(e.getMessage());
+        }
+        Long after = System.currentTimeMillis();
+        System.out.println("after:" + after);
+        long ti = after - before;
+        logger.info("cost time:" + String.valueOf(ti));
     }
 }
